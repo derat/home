88af05a95cfa894f967358bd07c386c44d13009b
Daniel Erat <dan@erat.org>
2017-01-08 11:11:56 -0700

    common: add source to Sample

--- common/sample_test.go
+++ common/sample_test.go
@@ -11,12 +11,14 @@ import (
 
 const DefaultTime int64 = 12345
 
-func parseString(str string, ut int64, name string, value float64) error {
+func parseString(str string, ut int64, source, name string, value float64) error {
 	var s Sample
 	if err := s.Parse(str, time.Unix(DefaultTime, 0)); err != nil {
 		return err
 	} else if s.Timestamp.Unix() != ut {
 		return fmt.Errorf("Expected timestamp %v; got %v", ut, s.Timestamp.Unix())
+	} else if s.Source != source {
+		return fmt.Errorf("Expected source %q; got %q", source, s.Source)
 	} else if s.Name != name {
 		return fmt.Errorf("Expected name %q; got %q", name, s.Name)
 	} else if s.Value != value {
@@ -25,26 +27,28 @@ func parseString(str string, ut int64, name string, value float64) error {
 	return nil
 }
 
-func createString(ut int64, name string, value float64) string {
-	s := Sample{time.Unix(ut, 0), name, value}
+func createString(ut int64, source, name string, value float64) string {
+	s := Sample{time.Unix(ut, 0), source, name, value}
 	return s.String()
 }
 
 func TestParse(t *testing.T) {
-	if err := parseString("123|TEMPERATURE|55.5", 123, "TEMPERATURE", 55.5); err != nil {
+	if err := parseString("123|BEDROOM|TEMPERATURE|55.5", 123, "BEDROOM", "TEMPERATURE", 55.5); err != nil {
 		t.Error(err)
 	}
-	if err := parseString("HUMIDITY|35", DefaultTime, "HUMIDITY", 35); err != nil {
+	if err := parseString("BATHROOM|HUMIDITY|35", DefaultTime, "BATHROOM", "HUMIDITY", 35); err != nil {
 		t.Error(err)
 	}
 
 	for _, str := range []string{
 		"",
-		"NAME",
-		"123|NAME",
-		"123|NAME|100.0|5",
-		"FOO|NAME|100.0",
-		"123|NAME|FOO",
+		"SOURCE",
+		"123|SOURCE",
+		"123|SOURCE|NAME",
+		"123|SOURCE|NAME|",
+		"123|SOURCE|NAME|100.0|5",
+		"FOO|SOURCE|NAME|100.0",
+		"123|SOURCE|NAME|FOO",
 	} {
 		var s Sample
 		if err := s.Parse(str, time.Unix(DefaultTime, 0)); err == nil {
@@ -54,8 +58,8 @@ func TestParse(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
-	const exp = "890|NAME|75.5"
-	s := Sample{time.Unix(890, 0), "NAME", 75.5}
+	const exp = "890|SOURCE|NAME|75.5"
+	s := Sample{time.Unix(890, 0), "SOURCE", "NAME", 75.5}
 	if str := s.String(); str != exp {
 		t.Errorf("Expected %q; got %q", exp, str)
 	}
