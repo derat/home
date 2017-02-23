b34576440b287451d901d24b57bc1ee3a598a822
Daniel Erat <dan@erat.org>
2017-02-23 06:25:18 -0700

    appengine: clean up and document summarize.go

--- appengine/storage/summarize.go
+++ appengine/storage/summarize.go
@@ -20,6 +20,105 @@ const (
 	summaryDeleteBatchSize = 500
 )
 
+// GenerateSummaries reads samples and inserts daily and hourly summary
+// entities. now.Location() is used to define day boundaries; hour boundaries
+// are computed based on UTC. fullDayDelay defines how long we wait after the
+// end of a day before assuming that we have all the data we're going to get
+// from it (and not re-summarizing it in the future).
+func GenerateSummaries(c context.Context, now time.Time, fullDayDelay time.Duration) error {
+	ct := now.Add(time.Duration(-1) * fullDayDelay)
+	partialDay := time.Date(ct.Year(), ct.Month(), ct.Day(), 0, 0, 0, 0, ct.Location())
+
+	// This could all be much simpler if it were possible to do a single query
+	// to get all samples, iterate through them in-order, and insert summaries
+	// in parallel while we go. However, App Engine appears to impose a
+	// five-second deadline on datastore RPCs, which is pretty easy to hit when
+	// summarizing multiple days' worth of samples. It's possible to get around
+	// this by grabbing a cursor and issuing a new query when near the deadline,
+	// but that leads to the second problem: datastore writes are extremely
+	// prone to failure, and become even more so when doing multiple writes in
+	// parallel.
+	//
+	// To mostly sidestep all of this garbage, issue a separate query for each
+	// day, insert summaries using sequential operations after reading the whole
+	// day, and mark the day as complete after summarizing it. This makes it
+	// more likely that we'll make forward progress when summarizing multiple
+	// days even if/when we hit a write error midway through.
+	dayStart := time.Time{}
+	if lfd, err := getSummaryLastFullDay(c); err != nil {
+		return err
+	} else if !lfd.IsZero() {
+		dayStart = lfd.In(now.Location()).AddDate(0, 0, 1)
+	}
+
+	for {
+		var err error
+		dayStart, err = summarizeDay(c, now.Location(), dayStart)
+		if err != nil {
+			return err
+		} else if dayStart.IsZero() {
+			break
+		}
+		log.Debugf(c, "Finished summarizing %4d-%02d-%02d",
+			dayStart.Year(), dayStart.Month(), dayStart.Day())
+
+		if dayStart.Before(partialDay) {
+			log.Debugf(c, "Marking %4d-%02d-%02d as fully summarized",
+				dayStart.Year(), dayStart.Month(), dayStart.Day())
+			k := datastore.NewKey(c, summaryStateKind, "", summaryStateId, nil)
+			if _, err := datastore.Put(c, k, &summaryState{dayStart}); err != nil {
+				return err
+			}
+		}
+
+		dayStart = dayStart.AddDate(0, 0, 1)
+	}
+	return nil
+}
+
+// DeleteSummarizedSamples deletes samples from days that have been "fully"
+// summarized (see GenerateSummaries). Samples from partially-summarized days
+// are never deleted. loc is used to determine day boundaries. daysToKeep
+// defines the number of fully-summarized days for which samples should be
+// retained.
+func DeleteSummarizedSamples(c context.Context, loc *time.Location, daysToKeep int) error {
+	lastFullDay, err := getSummaryLastFullDay(c)
+	if err != nil {
+		return err
+	} else if lastFullDay.IsZero() {
+		return nil
+	}
+	keepDay := lastFullDay.In(loc).AddDate(0, 0, 1-daysToKeep)
+
+	log.Debugf(c, "Deleting all samples earlier than %4d-%02d-%02d",
+		keepDay.Year(), keepDay.Month(), keepDay.Day())
+	q := datastore.NewQuery(sampleKind).KeysOnly().
+		Filter("Timestamp <", keepDay).Limit(summaryDeleteBatchSize)
+	for {
+		var keys []*datastore.Key
+		log.Debugf(c, "Querying for samples")
+		if keys, err = q.GetAll(c, nil); err != nil {
+			return err
+		} else if len(keys) == 0 {
+			break
+		}
+		log.Debugf(c, "Deleting %v sample(s)", len(keys))
+		if err = datastore.DeleteMulti(c, keys); err != nil {
+			return err
+		}
+		// If we didn't get a full set of keys, assume that this was the final
+		// delete. Otherwise, it looks like we can continue receiving query
+		// results for a while longer -- is this due to eventual consistency
+		// causing us to see already-deleted entities?
+		if len(keys) < summaryDeleteBatchSize {
+			break
+		}
+	}
+	return nil
+}
+
+// getSummaryLastFullDay queries datastore for the last fully-summarized day. It
+// returns an empty time.Time if no day has been fully summarized.
 func getSummaryLastFullDay(c context.Context) (time.Time, error) {
 	s := summaryState{}
 	k := datastore.NewKey(c, summaryStateKind, "", summaryStateId, nil)
@@ -29,6 +128,9 @@ func getSummaryLastFullDay(c context.Context) (time.Time, error) {
 	return s.LastFullDay, nil
 }
 
+// updateSummary incorporates an individual sample into a set of summaries. sums
+// contains existing summaries keyed by "source|name". ts contains the beginning
+// of the summarized time range.
 func updateSummary(sums map[string]*summary, sam *common.Sample, ts time.Time) {
 	sn := fmt.Sprintf("%s|%s", sam.Source, sam.Name)
 	if sum, ok := sums[sn]; ok {
@@ -53,6 +155,9 @@ func updateSummary(sums map[string]*summary, sam *common.Sample, ts time.Time) {
 	}
 }
 
+// writeSummaries performs batched datastore writes of hour and day summaries.
+// ds is keyed by "source|name", while hs's top-level keys are timestamps
+// describing the starts of summarized hourly ranges.
 func writeSummaries(c context.Context, ds map[string]*summary,
 	hs map[time.Time]map[string]*summary) error {
 	keys := make([]*datastore.Key, 0, summaryUpdateBatchSize)
@@ -157,101 +262,4 @@ func summarizeDay(c context.Context, loc *time.Location, queryStart time.Time) (
 	log.Debugf(c, "Processed %v samples in %v ms",
 		numSamples, getMsecSinceTime(startTime))
 	return dayStart, writeSummaries(c, daySums, hourSums)
-}
-
-// GenerateSummaries reads samples and inserts daily and hourly summary
-// entities. now.Location() is used to define day boundaries; hour boundaries
-// are computed based on UTC. fullDayDelay defines how long we wait after the
-// end of a day before assuming that we have all the data we're going to get
-// from it (and not re-summarizing it in the future).
-func GenerateSummaries(c context.Context, now time.Time, fullDayDelay time.Duration) error {
-	ct := now.Add(time.Duration(-1) * fullDayDelay)
-	partialDay := time.Date(ct.Year(), ct.Month(), ct.Day(), 0, 0, 0, 0, ct.Location())
-
-	// This could all be much simpler if it were possible to do a single query
-	// to get all samples, iterate through them in-order, and insert summaries
-	// in parallel while we go. However, App Engine appears to impose a
-	// five-second deadline on datastore RPCs, which is pretty easy to hit when
-	// summarizing multiple days' worth of samples. It's possible to get around
-	// this by grabbing a cursor and issuing a new query when near the deadline,
-	// but that leads to the second problem: datastore writes are extremely
-	// prone to failure, and become even more so when doing multiple writes in
-	// parallel.
-	//
-	// To mostly sidestep all of this garbage, issue a separate query for each
-	// day, insert summaries using sequential operations after reading the whole
-	// day, and mark the day as complete after summarizing it. This makes it
-	// more likely that we'll make forward progress when summarizing multiple
-	// days even if/when we hit a write error midway through.
-	dayStart := time.Time{}
-	if lfd, err := getSummaryLastFullDay(c); err != nil {
-		return err
-	} else if !lfd.IsZero() {
-		dayStart = lfd.In(now.Location()).AddDate(0, 0, 1)
-	}
-
-	for {
-		var err error
-		dayStart, err = summarizeDay(c, now.Location(), dayStart)
-		if err != nil {
-			return err
-		} else if dayStart.IsZero() {
-			break
-		}
-		log.Debugf(c, "Finished summarizing %4d-%02d-%02d",
-			dayStart.Year(), dayStart.Month(), dayStart.Day())
-
-		if dayStart.Before(partialDay) {
-			log.Debugf(c, "Marking %4d-%02d-%02d as fully summarized",
-				dayStart.Year(), dayStart.Month(), dayStart.Day())
-			k := datastore.NewKey(c, summaryStateKind, "", summaryStateId, nil)
-			if _, err := datastore.Put(c, k, &summaryState{dayStart}); err != nil {
-				return err
-			}
-		}
-
-		dayStart = dayStart.AddDate(0, 0, 1)
-	}
-	return nil
-}
-
-// DeleteSummarizedSamples deletes samples from days that have been "fully"
-// summarized (see GenerateSummaries). Samples from partially-summarized days
-// are never deleted. loc is used to determine day boundaries. daysToKeep
-// defines the number of fully-summarized days for which samples should be
-// retained.
-func DeleteSummarizedSamples(c context.Context, loc *time.Location, daysToKeep int) error {
-	lastFullDay, err := getSummaryLastFullDay(c)
-	if err != nil {
-		return err
-	} else if lastFullDay.IsZero() {
-		return nil
-	}
-	keepDay := lastFullDay.In(loc).AddDate(0, 0, 1-daysToKeep)
-
-	log.Debugf(c, "Deleting all samples earlier than %4d-%02d-%02d",
-		keepDay.Year(), keepDay.Month(), keepDay.Day())
-	q := datastore.NewQuery(sampleKind).KeysOnly().
-		Filter("Timestamp <", keepDay).Limit(summaryDeleteBatchSize)
-	for {
-		var keys []*datastore.Key
-		log.Debugf(c, "Querying for samples")
-		if keys, err = q.GetAll(c, nil); err != nil {
-			return err
-		} else if len(keys) == 0 {
-			break
-		}
-		log.Debugf(c, "Deleting %v sample(s)", len(keys))
-		if err = datastore.DeleteMulti(c, keys); err != nil {
-			return err
-		}
-		// If we didn't get a full set of keys, assume that this was the final
-		// delete. Otherwise, it looks like we can continue receiving query
-		// results for a while longer -- is this due to eventual consistency
-		// causing us to see already-deleted entities?
-		if len(keys) < summaryDeleteBatchSize {
-			break
-		}
-	}
-	return nil
 }
